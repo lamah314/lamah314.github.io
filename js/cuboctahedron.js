0865a7869461188d56d72b036470266c7e29c4eb
Anthony Lam <lamah314@gmail.com>
2019-02-22 23:22:59 -0500

    figured out animationsgit add .! :D

--- js/cuboctahedron.js
+++ js/cuboctahedron.js
@@ -1,28 +1,70 @@
-window.onload = function() {
+window.onload = function () {
     var cuboctahedron = document.querySelector('#cuboctahedron');
-// var navList = document.querySelector('.nav-list');
-var button = document.getElementById('right-button');
-// var currentClass = '';
+    var buttonFront = document.getElementsByClassName('front-button');
+    var buttonRight = document.getElementsByClassName('right-button');
+    var buttonLeft = document.getElementsByClassName('left-button');
+    var newTrans = '';
+    var currentClass = '';
 
+    for (var i = 0; i < buttonFront.length; i++) {
+        buttonFront.item(i).onclick = changeToFrontSide;
+    }
 
-button.onclick= changeSide;
+    for (var i = 0; i < buttonRight.length; i++) {
+        buttonRight.item(i).onclick = changeToRightSide;
+    }
 
-// button.onclick = changeSide();
-// document.getElementById('right-button').onclick = document.getElementById('cuboctahedron').style.transform = 'translateZ(-353.553390593px) rotateY( -90deg)';
+    for (var i = 0; i < buttonLeft.length; i++) {
+        buttonLeft.item(i).onclick = changeToLeftSide;
+    }
 
+    function changeToFrontSide() {
+        if (newTrans) {
+            document.documentElement.style.setProperty('--current-trans', newTrans);
+        }
+        if (currentClass) {
+            cuboctahedron.classList.remove(currentClass);
+        }
+        newTrans = "translateZ(-353.553390593px) rotateY( 0deg) rotateZ(45deg)";
+        document.documentElement.style.setProperty('--new-trans', newTrans);
+        cuboctahedron.classList.add("show-front");
+        currentClass = "show-front";
+    }
 
+    function changeToRightSide() {
+        if (newTrans) {
+            document.documentElement.style.setProperty('--current-trans', newTrans);
+        }
+        if (currentClass) {
+            cuboctahedron.classList.remove(currentClass);
+        }
+        newTrans = "translateZ(-353.553390593px) rotateY( -90deg)";
+        document.documentElement.style.setProperty('--new-trans', newTrans);
+        console.log(cuboctahedron.className);
+        cuboctahedron.classList.add("show-right");
+        currentClass = "show-right";
+    }
+
+    function changeToLeftSide() {
+        if (newTrans) {
+            document.documentElement.style.setProperty('--current-trans', newTrans);
+        }
+        if (currentClass) {
+            cuboctahedron.classList.remove(currentClass);
+        }
+        newTrans = "translateZ(-353.553390593px) rotateY( 90deg)";
+        document.documentElement.style.setProperty('--new-trans', newTrans);
+        console.log(cuboctahedron.className);
+        cuboctahedron.classList.add("show-left");
+        currentClass = "show-left";
+    }
+
+    function moveItemBack(x) {
+        x.style.transfrom = 'translateZ(-1000px)';
+    }
+
+    function moveItemForward(x) {
+        x.style.transfrom = 'translateZ(1000px)';
+    }
 
 };
-
-function changeSide() {
-    // document.getElementById("cuboctahedron").style.transform = "rotateY( -90deg)";
-    // cuboctahedron.classList.add("cuboctahedron.show-right");
-    // document.getElementById("cuboctahedron").classList.add("cuboctahedron.show-right");
-    cuboctahedron.style.transform = 'translateZ(-353.553390593px) rotateY( -90deg)';
-    //   var showClass = 'show-' + button.value;
-    //   if ( currentClass ) {
-    //     cuboctahedron.classList.remove( currentClass );
-    //   }
-    //   cuboctahedron.classList.add( showClass );
-    //   currentClass = showClass;
-}
