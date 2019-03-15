e802400836ea1dd0d5713d2a1f8635f0eaf730fd
Anthony Lam <lamah314@gmail.com>
2019-03-14 21:58:01 -0400

    added images

--- js/modal.js
+++ js/modal.js
@@ -1,6 +1,9 @@
 // Get the modal
 let modal = document.querySelector('#myModal-img');
 
+// Get modal image
+let modal2 = document.querySelector('#modal-img');
+
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 let img = document.querySelectorAll('.AboutMe__img');
 let modalImg = document.querySelector("#modal-img");
@@ -13,6 +16,11 @@ for (let i = 0; i < img.length; i++) {
         captionText.innerHTML = this.alt;
     }
 }
+
+modal2.onclick = function () {
+    let imageSrc = this.src.split(".")
+    modalImg.src = imageSrc[0] + '1' + imageSrc[1]
+}
 // Get the <span> element that closes the modal
 let span = document.getElementsByClassName("close")[0];
 
@@ -20,3 +28,4 @@ let span = document.getElementsByClassName("close")[0];
 span.onclick = function () {
     modal.style.display = "none";
 }
+
